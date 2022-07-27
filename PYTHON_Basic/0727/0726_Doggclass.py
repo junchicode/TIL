db623274ee35b7275ae7e7c6ff44ca2c81978382
junchicode <junchicode@gmail.com>
2022-07-27 17:28:21 +0900

    0727

---
+++ PYTHON_Basic/0727/0726_Doggclass.py
@@ -0,0 +1,23 @@
+class Doggy:
+    
+    num_of_dogs = 0 # 클래스 변수
+    birth_of_dogs = 0 # 클래스 변수
+
+    def __init__(self, name, bread): # 생성자
+        self.name = name
+        self.bread = bread 
+        Doggy.num_of_dogs += 1
+        Doggy.birth_of_dogs += 1   
+    
+    def bark(self):
+        return '멍'
+    
+    def __del__(self):
+        Doggy.num_of_dogs -= 1
+    
+    def get_status(cls):
+        return f'Birth : {cls.birth_of_dogs}, Current count: {cls.num_of_dogs}'
+    
+dog1 = Doggy('아리','포메라니안')
+print(Doggy.get_status(dog1))
+    
