6b7bd75675682481d09150a0810110d3c76f9dcb
junchicode <junchicode@gmail.com>
2022-09-16 00:18:04 +0900

    0916

--- Algo_week_SWEA&Class/week4/0915/tree2.py
+++ Algo_week_SWEA&Class/week4/0915/tree2.py
@@ -8,6 +8,11 @@
 for j in range(0, E*2, 2):
 p, c = arr[j], arr[j+1]
 '''
+def find_root(V):
+    for i in range(1, V+1):
+        if par[i] == 0: # 부모가 없으면 
+            return i
+
 def preorder(n): # 전위
     if n:
         print(n) # visit(n)
@@ -26,27 +31,22 @@ def postorder(n): # 후위
         postorder(ch2[n])
         print(n) # visit(n)
 
-root = 1 # 루트를 1이라 가정
 E = int(input())
 arr = list(map(int, input().split()))
 V = E + 1
 # 부모를 인덱스로 자식 번호 저장
 ch1 = [0]*(V+1)
 ch2 = [0]*(V+1)
-
+par = [0]*(V+1) # 자식을 인덱스로 자식 번호 저장
 for i in range(E):
     p, c = arr[i*2], arr[i*2+1]
 
     if ch1[p] == 0: # 아직 자식이 없으면
-        ch1[p] = c  # 첫번째 자식으로
+        ch1[p] = c  # 첫번째 자식으로 저장
     else:
         ch2[p] = c
-print(ch1)
-print(ch2)
-    
-preorder(root)
-print()
-inorder(root)
-print()
-postorder(root)
-print()
+    par[c] = p
+root = find_root(V)
+print(par)
+print(root)
+
--- Algo_week_SWEA&Class/week4/0915/tree_prac_1_2.py
+++ Algo_week_SWEA&Class/week4/0915/tree_prac_1_2.py
@@ -1,38 +1,36 @@
-'''
-정점 번호 V = 1~(E+1)
-간선 수 
-부모-자식 순
-4
-1 2 1 3 3 4 3 5
+# 3을 루트로 하는 서브트리에 속한 정점의 개수는?
+def find_root(V):
+    for i in range(1, V+1):
+        if par[i] == 0: # 부모가 없으면 
+            return i
 
-for j in range(0, E*2, 2):
-p, c = arr[j], arr[j+1]
-'''
 def preorder(n): # 전위
+    global cnt
     if n:
-        print(n) # visit(n)
+        cnt += 1 #print(n, end = " ") # visit(n)
         preorder(ch1[n])
         preorder(ch2[n])
 
 def inorder(n): # 중위
     if n:
         inorder(ch1[n])
-        print(n) # visit(n)
+        print(n,end = " ") # visit(n)
         inorder(ch2[n])
 
 def postorder(n): # 후위
     if n:
         postorder(ch1[n])
         postorder(ch2[n])
-        print(n) # visit(n)
+        print(n, end = " ") # visit(n)
 
 root = 1 # 루트를 1이라 가정
-E = int(input())
+V = int(input())
 arr = list(map(int, input().split()))
-V = E + 1
+E = V - 1
 # 부모를 인덱스로 자식 번호 저장
 ch1 = [0]*(V+1)
 ch2 = [0]*(V+1)
+par = [0]*(V+1)
 
 for i in range(E):
     p, c = arr[i*2], arr[i*2+1]
@@ -41,12 +39,14 @@ for i in range(E):
         ch1[p] = c  # 첫번째 자식으로
     else:
         ch2[p] = c
-print(ch1)
-print(ch2)
-    
-preorder(root)
-print()
+    par[c] = p
+
+root = find_root(V)
+cnt = 0
+
+preorder(3)
+print(cnt)
+'''print()
 inorder(root)
 print()
-postorder(root)
-print()
+postorder(root)'''
