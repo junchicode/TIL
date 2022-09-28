2894bb664a25b5e85b3d50f106b9fb653a649342
junchicode <junchicode@gmail.com>
2022-09-28 22:11:22 +0900

    0928

--- Algo_week_SWEA&Class/week6/0927/swea_5207.py
+++ Algo_week_SWEA&Class/week6/0927/swea_5207.py
@@ -1,26 +1,38 @@
-def bin_s(arr, target, start, end):
-    if start > end:
-        return None
-    mid = (start+end) // 2
-    # 찾은 경우 중간점 인덱스 반환
-    if A[mid] == target:
-        return mid
-    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-    elif A[mid] > mid:
-        return bin_s(A, target, start, mid-1)
-    else:
-        return bin_s(A, target, mid+1, end)
-    
-T = int(input())
-for tc in range(1, T+1):
+for t in range(int(input())):
+    answer = 0
     N, M = map(int, input().split())
     A = sorted(list(map(int, input().split())))
     B = list(map(int, input().split()))
-    ans = 0
     for b in B:
-        if bin_s(A, b, 0, N-1):
-            ans += 1
-    print(ans)
+        # 중간을 탐색하기 때문에 최소점과 최대점을 잡아준다.
+        min_idx = 0
+        max_idx = N - 1
+        # 왼쪽으로 갔는지 오른쪽으로 갔는지 체크하기 위한 용도
+        flag = 0
+        # 탐색범위가 같아지거나 엇갈릴때까지 반복한다.
+        while min_idx <= max_idx:
+            # 중간지점의 위치를 구한다.
+            avg_idx = (min_idx+max_idx)//2
+            # 중간지점의 값이 정답이라면 answer를 늘리고 반복문을 중지한다.
+            if A[avg_idx] == b:
+                answer += 1
+                break
+            # 선택한 인덱스 값이 찾는값보다 크다면
+            elif A[avg_idx] > b:
+                # 선택한 인덱스 위쪽은 필요없으므로 최대 인덱스 위치를 수정한다.
+                max_idx = avg_idx - 1
+                # 이전에도 왼쪽을 선택했다면 중지
+                if flag == 1: break
+                # 왼쪽으로 갔다고 표시한다.
+                flag = 1
+            else:
+                # 반대의 상황이라면 최소값을 바꿔준다.
+                min_idx = avg_idx + 1
+                # 이전에 오른쪽으로 갔다면 중지
+                if flag == -1: break
+                # 오른쪽으로 갔다고 표시한다.
+                flag = -1
+    print('#{} {}'.format(t+1, answer))
     
                     
     
